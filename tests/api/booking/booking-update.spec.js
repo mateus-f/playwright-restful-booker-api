83a8f5047f1f8b90308eb9641b07a84216b3c6e0
Mateus Ferreira <mateusjunior803@gmail.com>
2026-09-16 22:59:12 -0300

    🐛 fix(booking): update authentication parameter usage

--- tests/api/booking/booking-update.spec.js
+++ tests/api/booking/booking-update.spec.js
@@ -164,10 +164,10 @@ test.describe("Atualização de uma reserva", () => {
       return BookingFactory.createBookingPayload();
     });
 
-    const authToken = await test.step("And que eu não informe credenciais de autenticação", () => null);
+    await test.step("And que eu não informe credenciais de autenticação", () => null);
 
     const updateBookingResponse = await test.step(`When eu enviar uma requisição "PUT" para a rota "/booking/${bookingId}"`, async () => {
-      return bookingService.updateBooking(bookingId, validBookingUpdatePayload, authToken, false);
+      return bookingService.updateBooking(bookingId, validBookingUpdatePayload, "none");
     });
 
     await test.step("Then o código de status HTTP retornado deve ser 403", () => {
